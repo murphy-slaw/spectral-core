c0d14299fc5942c06b4d7304c8b8593bb90237bf
Kevin Murphy <kevin@eorbit.net>
2026-08-18 07:32:00 -0700

    Revert "Add structure location fix"
    This reverts commit e94d829714b929715d0ba60c692c60f43e5dd7fe.

--- fabric/src/main/java/net/funkpla/spectral_core/ChunkGeneratorStructureStateAccessor.java
+++
@@ -1,7 +0,0 @@
-package net.funkpla.spectral_core;
-
-import net.minecraft.world.level.chunk.ChunkGeneratorStructureState;
-
-public interface ChunkGeneratorStructureStateAccessor {
-	void passGeneratorState(ChunkGeneratorStructureState state);
-}
